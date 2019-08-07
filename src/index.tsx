3bc0a86572009188ee9ad6196df6d112c124f5cc
박기완 <zprime0920@gmail.com>
2019-08-07 15:20:23 +0900

    Chore: styled-components 설치, 기본 코드 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers'
+import GlobalStyle from './GlobalStyle';
 
 declare global {
   interface Window {
@@ -21,6 +21,7 @@ const store = createStore(
 ReactDOM.render(
   (
     <Provider store={store}>
+      <GlobalStyle />
       <App />
     </Provider>
   ),
