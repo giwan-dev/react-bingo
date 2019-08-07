cca7197e2e51fa39659f480359c9ed9e185c2633
박기완 <zprime0920@gmail.com>
2019-08-08 02:29:57 +0900

    Updates

---
+++ precache-manifest.82476d9fbcb06c9d725c1104fa7003fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f079057dc310f929ef4aa3116bb450",
+    "url": "/react-bingo/index.html"
+  },
+  {
+    "revision": "112a35a259db73c8b542",
+    "url": "/react-bingo/static/js/2.4c429690.chunk.js"
+  },
+  {
+    "revision": "bbf264e2c319dcc44458",
+    "url": "/react-bingo/static/js/main.e1e38143.chunk.js"
+  },
+  {
+    "revision": "1f70a92c7d62d12b28c4",
+    "url": "/react-bingo/static/js/runtime~main.36f22ad7.js"
+  }
+]);
