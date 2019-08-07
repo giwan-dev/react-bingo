806659e28676ab31ac7cc5f8fec43fa6afc902b3
박기완 <zprime0920@gmail.com>
2019-08-07 15:57:55 +0900

    Fix: lint 에러 수정

--- src/reducers.ts
+++ src/reducers.ts
@@ -1,7 +1,7 @@
 const initialState = {};
 
 export default function rootReducer(state = initialState, action: any) { // FIXME: 액션 타입 가져오기
-  switch(action.type) {
+  switch (action.type) {
     default:
       return state;
   }
