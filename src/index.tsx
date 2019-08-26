eb4ab77c0d59a774d46826e737c13fc4188c1dab
박기완 <zprime0920@gmail.com>
2019-08-26 17:37:48 +0900

    Feature: GlobalStyle 컴포넌트 테스트 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import GlobalStyle from './components/GlobalStyle';
+import GlobalStyle from 'components/GlobalStyle';
 import store from 'store';
 
 ReactDOM.render(
